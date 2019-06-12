7fe21212c2317613edad06f8c6e014df66624165
mchandrika <mchandrika@manh.com>
2019-06-12 19:22:15 +0530

    changes for form and table

--- medical-app/src/app/app-routing.module.ts
+++ medical-app/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'family-history', loadChildren: './family-history/family-history.module#FamilyHistoryPageModule' },
   //{ path: 'form', loadChildren: './pages/form/form.module#FormPageModule' },
   { path: 'student-profile', loadChildren: './pages/student-profile/student-profile.module#StudentProfilePageModule' },
-  { path: 'diagnosis', loadChildren: '.pages/diagnosis/diagnosis.module#DiagnosisPageModule' },
+  { path: 'diagnosis', loadChildren: './pages/diagnosis/diagnosis.module#DiagnosisPageModule' },
   // { path: 'present-medical-history', loadChildren: './diagnosis/components/present-medical-history/present-medical-history.module#PresentMedicalHistoryPageModule' },
   // { path: 'anthropometric-measeurements', loadChildren: './diagnosis/components/anthropometric-measeurements/anthropometric-measeurements.module#AnthropometricMeaseurementsPageModule' },
   { path: 'students', loadChildren: './pages/students/students.module#StudentsPageModule' }
