6f074a236ddf89892b346013ca3d9d369bb9c5e8
Akruti Kushwaha <ask.aakriti30@gmail.com>
2019-06-13 00:24:50 +0530

    Worked on Sidebar, Toolbar(Inprogress)

--- medical-app/src/app/pages/sidemenu/sidemenu.page.ts
+++ medical-app/src/app/pages/sidemenu/sidemenu.page.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidemenuPage implements OnInit {
     public appPages: any;
+    public imgURL ="../../assets/images/logo.png";
   constructor() {
     this.appPages = [
   {
